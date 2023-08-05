ba250235a9add8de27b3a6fce39f3a79f37e534d
KAtUCSC <97263808+KiraEatsMetal@users.noreply.github.com>
2023-08-05 01:10:48 -0700

    CODE BITCHES

--- starbound/guncalc/js/guncalc.js
+++ starbound/guncalc/js/guncalc.js
@@ -7,7 +7,7 @@
 //t5: 150
 //unarmoured: 100
 //const: energy per tier, recharge time 1.75, recharge block time 1.5
-//vars: base dps, energyUsage, fire time
+//lets: base dps, energyUsage, fire time
 
 //energy per Shot = eU * fireTime
 //damage per Shot = dps * fireTime
@@ -20,8 +20,31 @@
 update()
 
 function update(){
+  console.log("started")
   //stuff here
+  let output = document.getElementById("output");
+
+  let maxenergy = document.getElementById("max-energy").value;
+  let blocktime = Number(document.getElementById("block-time").value);
+  let regentime = Number(document.getElementById("regen-time").value);
+  
+  let energyusage = document.getElementById("energy-usage").value;
+  let dps = document.getElementById("dps").value;
+  let firetime = document.getElementById("fire-time").value;
+
+  let eShot = energyusage * firetime;
+  let dShot = dps * firetime;
+
+  let sBar = Math.ceil(maxenergy/eShot);
+  let dBar = sBar * dShot;
+
+  let tDps = ((dShot * sBar) / ((firetime * sBar) + blocktime + regentime));
+  let tDpsRegen =  ((dShot * sBar) / ((firetime * sBar) + regentime));
 
   //update periodically
-  setTimeout(update, 1500);
+  let final = ": "+String(sBar)+"<br>: "+String(dBar)+"<br>: "+String(tDps)+"<br>: "+String(tDpsRegen);
+  console.log(final);
+  output.innerHTML = final;
+  setTimeout(update, 3000);
+  console.log("repeating");
 }
