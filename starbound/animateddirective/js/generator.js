a74c4e53e43d475669b0aa39778bcba67200c951
KAtUCSC <97263808+KiraEatsMetal@users.noreply.github.com>
2024-01-02 19:56:29 -0800

    generator prep work
    some html (link crediting rexmeck for the original generator), partially cannibalized from rexmeck's site
    added two js files from rexmeck's github

---
+++ starbound/animateddirective/js/generator.js
@@ -0,0 +1,7 @@
+/*
+get y height from image
+ask user for frame count
+frame width is image x length / frame count, remove the remainder
+for each frame, set green value to frame number and run directive generator
+return conglomerated replace string
+*/
